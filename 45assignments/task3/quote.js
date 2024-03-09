264fca19f0123bd3addf25d3c552515da2783f52
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-09 13:27:32 +0500

    task3 was added

---
+++ 45assignments/task3/quote.js
@@ -0,0 +1,3 @@
+var author = "Itachi Uchiha";
+var quote = "'It is foolish to fear what we have yet to see and know'";
+console.log(author, "once said,", quote);
---
+++ 45assignments/task3/quote.ts
@@ -0,0 +1,3 @@
+let author ="Itachi Uchiha"
+let quote ="'It is foolish to fear what we have yet to see and know'"
+console.log(author ,"once said,", quote )
