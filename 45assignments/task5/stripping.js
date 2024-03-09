bd5a892da909c503c631e43b53684bbb9c875bd4
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-09 15:41:26 +0500

    task5 was added

---
+++ 45assignments/task5/stripping.js
@@ -0,0 +1,4 @@
+var calisthenicsidol = "\n\t Ian berseagle ";
+console.log("Calisthenics idols with whitespaces: ", calisthenicsidol);
+var calisthenicslegends = calisthenicsidol.trim();
+console.log("Calisthenics legends without whitespaces: ", calisthenicslegends);
---
+++ 45assignments/task5/stripping.ts
@@ -0,0 +1,4 @@
+let calisthenicsidol="\n\t Ian berseagle "
+console.log("Calisthenics idols with whitespaces: ",calisthenicsidol)
+let calisthenicslegends= calisthenicsidol.trim()
+console.log("Calisthenics legends without whitespaces: ", calisthenicslegends)
