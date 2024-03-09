17bf90da5a33c806b23c8bb4a67fb754243c6280
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-09 11:40:23 +0500

    task1 added

---
+++ 45assignments/task1/main.js
@@ -0,0 +1,4 @@
+var friend = "Moiz,";
+var message1 = "hello!";
+var message2 = "would you like to learn typescript?";
+console.log(message1, friend, message2);
---
+++ 45assignments/task1/main.ts
@@ -0,0 +1,4 @@
+let friend:string="Moiz,"
+let message1:string="hello!"
+let message2:string="would you like to learn typescript?"
+console.log(message1,friend,message2)
