13505982f5a613187e2b0bef0dcb04a2309ded51
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-11 16:01:46 +0500

    task13 was added

---
+++ 45assignments/task13/transportation.js
@@ -0,0 +1,7 @@
+var transport = ["Camry", "Mustang", "Defy"];
+var com1msg = "belongs to \"Toyota\"";
+var com2msg = "belongs to \"Ford\"";
+var com3msg = "belongs to \"Giant\"";
+console.log(transport[0], com1msg);
+console.log(transport[1], com2msg);
+console.log(transport[2], com3msg);
---
+++ 45assignments/task13/transportation.ts
@@ -0,0 +1,7 @@
+let transport= ["Camry","Mustang","Defy"]
+let com1msg="belongs to \"Toyota\""
+let com2msg="belongs to \"Ford\""
+let com3msg="belongs to \"Giant\""
+console.log(transport[0],com1msg)
+console.log(transport[1],com2msg)
+console.log(transport[2],com3msg)
