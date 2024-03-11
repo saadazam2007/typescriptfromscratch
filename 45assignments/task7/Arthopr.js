5da71aa4821d4d78190b5b5bc964213cad5bf14f
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-11 11:57:39 +0500

    task7 was added

---
+++ 45assignments/task7/Arthopr.js
@@ -0,0 +1,17 @@
+//for addition
+var num1 = 3;
+var num2 = 5;
+var sum = num1 + num2;
+//for subtraction
+var num3 = 18;
+var num4 = 10;
+var sub = num3 - num4;
+//for multiplication
+var num5 = 2;
+var num6 = 4;
+var mult = num5 * num6;
+//for division
+var num7 = 16;
+var num8 = 2;
+var div = num7 / num8;
+console.log('\n\t', sum, '\n\n\t', sub, '\n\n\t', mult, '\n\n\t', div);
---
+++ 45assignments/task7/Arthopr.ts
@@ -0,0 +1,20 @@
+//for addition
+let num1 = 3
+let num2 = 5
+let sum = num1 + num2
+
+//for subtraction
+let num3 = 18
+let num4 = 10
+let sub = num3 - num4
+
+//for multiplication
+let num5 = 2
+let num6 = 4
+let mult= num5 * num6
+
+//for division
+let num7 = 16
+let num8 = 2
+let div = num7 / num8
+console.log( '\n\t',sum ,'\n\n\t', sub ,'\n\n\t', mult ,'\n\n\t',div)
