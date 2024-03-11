f0fdd7327c09544f304de1f9ff1ea321134bd4b1
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-11 16:14:01 +0500

    task14 was added

---
+++ 45assignments/task14/guestls.js
@@ -0,0 +1,5 @@
+var guestlist = { name1: "Hannan,", name2: "Kaab,", name3: "Musab," };
+var msg = "Can you have dinner with me the day after tomorrow";
+console.log(guestlist.name1, msg);
+console.log(guestlist.name2, msg);
+console.log(guestlist.name3, msg);
---
+++ 45assignments/task14/guestls.ts
@@ -0,0 +1,5 @@
+let guestlist = {name1:"Hannan,", name2:"Kaab,", name3:"Musab,"}
+let msg= "Can you have dinner with me the day after tomorrow"
+console.log(guestlist.name1,msg)
+console.log(guestlist.name2,msg)
+console.log(guestlist.name3,msg)
