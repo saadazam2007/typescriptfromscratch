a418304f126e86516da6e81033596eb52447ee71
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-11 12:20:33 +0500

    task10 was added

---
+++ 45assignments/task10/comments.js
@@ -0,0 +1,4 @@
+var my_Friend = "Hannan";
+var me = "Saad";
+console.log(my_Friend, "is", me, "best friend");
+// this prints the two names stored in both variables and show them in result
---
+++ 45assignments/task10/comments.ts
@@ -0,0 +1,4 @@
+let my_Friend = "Hannan"
+let me = "Saad"
+console.log(my_Friend ,"is", me , "best friend")
+// this prints the two names stored in both variables and show them in result
