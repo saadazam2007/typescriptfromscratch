b22c6fb0dba5e6bd8a3c09a2ee58d76a1a7f4299
learningtypescriptfromscratch <saadazamkhan5900@gmail.com>
2024-03-11 15:47:51 +0500

    task12 was added

---
+++ 45assignments/task12/arraymsg.js
@@ -0,0 +1,6 @@
+var friendnames = [
+    "Moiz", "Anas", "Aqib"
+];
+console.log("Assalamualaikum", friendnames[0]);
+console.log("Assalamualaikum", friendnames[1]);
+console.log("Assalamualaikum", friendnames[2]);
